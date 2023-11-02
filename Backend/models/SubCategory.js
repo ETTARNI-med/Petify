ea5d9ab04589b98329f1b1b9297a1c4bf08a7d8f
khalidloka <96070737+khalidloka@users.noreply.github.com>
2023-11-02 10:28:01 +0100

    push models

--- Backend/models/SubCategory.js
+++ Backend/models/SubCategory.js
@@ -1,28 +1,28 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const SubCategorySchema = new mongoose.Schema(
-//   {
-//     _id: Schema.Types.UUID,
-//     subcategory_name: {
-//       type: String,
-//       require: true,
-//       min: 2,
-//       max: 60,
-//     },
-//     category_id: {
-//       type: String,
-//       require: true,
-//     },
-//     active: {
-//       type: Boolean,
-//       default: false,
-//     },
-//   },
-//   {
-//     timestamps: false,
-//   }
-// );
+const SubCategorySchema = new mongoose.Schema(
+  {
+    _id: Schema.Types.UUID,
+    subcategory_name: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+    },
+    category_id: {
+      type: String,
+      require: true,
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
 
-// const SubCategory = mongoose.model("SubCategory", SubCategorySchema);
+const SubCategory = mongoose.model("SubCategory", SubCategorySchema);
 
-// export default SubCategory;
+export default SubCategory;
