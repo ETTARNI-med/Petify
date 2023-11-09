ef3c7c5866cd6489e8e729a5fd601e19ac79ac9c
khalidloka <96070737+khalidloka@users.noreply.github.com>
2023-11-09 16:08:39 +0100

    change some stuffs
    isensitive , toLower case

--- Backend/controllers/checkAuth.js
+++ Backend/controllers/checkAuth.js
@@ -1,73 +1,76 @@
 //check register customer
 
-const { generateToken } = require('../config/jwtToken');
-const Customer = require('../models/CustomerSchema');
+const { generateToken } = require("../config/jwtToken");
+const Customer = require("../models/CustomerSchema");
 
-const asyncHandler=require('express-async-handler');
+const asyncHandler = require("express-async-handler");
 
-const registerCustomer = asyncHandler(
-    async(req,res)=>{
-        const email = req.body.email;
-        const username = req.body.username;
-    
-        const findCustomer = await Customer.findOne({email})
-        if (!findCustomer) {
-          const newCustomer = await Customer.create(req.body)
+const registerCustomer = asyncHandler(async (req, res) => {
+  const email = req.body.email;
+  const username = req.body.username;
 
-          res.status(200).json(req.body);
-    
-        } else {
-            throw new Error("User already exists")
-       }
-        
-    }
-)
+  const findCustomer = await Customer.findOne({ email });
+  if (!findCustomer) {
+    const newCustomer = await Customer.create(req.body);
+
+    res.status(200).json(req.body);
+  } else {
+    throw new Error("User already exists");
+  }
+});
 
 //function Login
 
-const login = asyncHandler( async (req,res)=>{
-  const { email, password}=req.body;
-         //check if user exist
-  const findCustomer = await Customer.findOne({email});
-  if(findCustomer&& await findCustomer.isPasswordMatched(password)) {
-res.json({
-
-  _id:findCustomer?._id,
-  username:findCustomer?.username,
-  lastname:findCustomer?.lastname,
-  firstname:findCustomer?.firstname,
-  email:findCustomer?.email,
-  active:true,
-token:generateToken(findCustomer?._id),
-})
-}else{
-  throw new Error("Invalid Credentials")}
-})
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  //check if user exist
+  const findCustomer = await Customer.findOne({ email });
+  if (findCustomer && (await findCustomer.isPasswordMatched(password))) {
+    res.json({
+      _id: findCustomer?._id,
+      username: findCustomer?.username,
+      lastname: findCustomer?.lastname,
+      firstname: findCustomer?.firstname,
+      email: findCustomer?.email,
+      active: true,
+      token: generateToken(findCustomer?._id),
+    });
+  } else {
+    throw new Error("Invalid Credentials");
+  }
+});
 
 // Get all users
 
-
-
 //search for a customer
 const searchCustomer = asyncHandler(async (req, res) => {
-  const { query } = req.query;
+  let { query } = req.query;
 
-  if(typeof query === "undefined"){
+  if (typeof query === "undefined") {
     try {
       const getCustomers = await Customer.find();
       res.json(getCustomers);
     } catch (error) {
       throw new Error(error);
     }
-  }else{
+  } else {
     try {
+      query = query.toLocaleLowerCase();
+      const regex = new RegExp(query, "i");
+
       // Find the customer based on the search query
-      const customer = await Customer.find({$or:[{first_name:query},{last_name:query},{email:query}]});
-  
+      const customer = await Customer.find({
+        $or: [
+          { first_name: { $regex: regex } },
+          { last_name: { $regex: regex } },
+          { email: { $regex: regex } }
+        ],
+      });
+
       if (!customer) {
-        return res.status(404).json({ message: 'Customer not found' });
+        return res.status(404).json({ message: "Customer not found" });
       }
-  
+
       res.json({
         customer,
       });
@@ -75,39 +78,39 @@ const searchCustomer = asyncHandler(async (req, res) => {
       throw new Error(error);
     }
   }
-  
 });
-
 
 //search for a customer by id
-const customerById = asyncHandler( async (req,res)=>{
-const {id}=req.params;
-try{
-
-const getCustomer = await Customer.findById(id);
-res.json({
-  getCustomer,
-});
-} catch(error){
-  throw new Error(error);
-}
-}
-);
-//delete a customer
-const deleteCustomer = asyncHandler( async (req,res)=>{
-
-  const Active = req.body.active;
-  try{
-    if(Active){
-
-  const deletCustomer = await Customer.findById(req.body.id);
-  res.json({
-    deletCustomer,
-  });}
-  } catch(error){
+const customerById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const getCustomer = await Customer.findById(id);
+    res.json({
+      getCustomer,
+    });
+  } catch (error) {
     throw new Error(error);
   }
+});
+//delete a customer
+const deleteCustomer = asyncHandler(async (req, res) => {
+  const Active = req.body.active;
+  try {
+    if (Active) {
+      const deletCustomer = await Customer.findById(req.body.id);
+      res.json({
+        deletCustomer,
+      });
+    }
+  } catch (error) {
+    throw new Error(error);
   }
-  );
+});
 
-module.exports = {registerCustomer,login,customerById,deleteCustomer,searchCustomer};
+module.exports = {
+  registerCustomer,
+  login,
+  customerById,
+  deleteCustomer,
+  searchCustomer,
+};
