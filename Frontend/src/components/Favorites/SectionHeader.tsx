a1af9b88d46601ed4352824e32a25fa37b7329c0
labgenetic <Charafcharouit@gmail.com>
2023-11-24 11:54:45 +0100

    Need to adjust breadcrumps

--- Frontend/src/components/Favorites/SectionHeader.tsx
+++ Frontend/src/components/Favorites/SectionHeader.tsx
@@ -1,11 +1,12 @@
+import { LucideFolderHeart } from "lucide-react";
+
 
 export default function SectionHeader({subtitle, title}: {subtitle: string; title: string}) {
   return (
     <header className="px-10 mb-6">
       <div>
         <div className="flex items-center justify-start relative h-12 gap-x-4 mb-6">
-          <div className="h-full w-6 bg-secondcolor rounded-md"></div>
-          <h5 className="font-bold text-secondcolor text-xl">{subtitle}</h5>
+          <h5 className="flex gap-1 md:gap-2 xl:gap-4 font-bold text-secondcolor text-xl items-center"><LucideFolderHeart size={30}/>{subtitle}</h5>
         </div>
         <h2 className="dark:text-white text-4xl uppercase font-bold text-gray-950">{title}</h2>
       </div>
