de28390c80348862cc6149d0f7642517b23ce937
labgenetic <Charafcharouit@gmail.com>
2023-12-09 07:49:45 +0100

    adjusred Breadcrumps to accept an array of objects view profile or wishlist

--- Frontend/src/components/BreadCrumps/Breadcrumps.tsx
+++ Frontend/src/components/BreadCrumps/Breadcrumps.tsx
@@ -1,24 +1,40 @@
 import React from "react";
 import Breadcrump from "./Breadcrump";
 
-interface DirectoryItem {
+interface BreadcrumpsProps {
+  Directory: Array<{
     path: string;
-    logo: React.ReactNode; // Assuming logo is a string, you can replace it with the actual type
+    logo: Element;
     title: string;
-  }
-  
-  interface BreadcrumpsProps {
-    Directory: DirectoryItem[];
-  }
-export default function Breadcrumps ({BreadcrumpsProps}:{} ) {
+  }>;
+}
+
+const Breadcrumps: React.FC<BreadcrumpsProps> = ({ Directory }) => {
   return (
     <nav aria-label="Breadcrumb">
-      <ol className="flex items-center gap-1 text-sm text-gray-600">{
-        Directory.map((item,index)=>{
-            <li key={index}><Breadcrump path={item.path} logo={item.logo} title={item.title}/></li>
-        })
-      }
+      <ol className="flex items-center gap-1 text-gray-600">
+        {Directory.map((item, index) => (
+          <li key={index} className="flex items-center">
+            <Breadcrump path={item.path} logo={item.logo} title={item.title} />
+            {index !== Directory.length - 1 && ( // Check if it's not the last element
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            )}
+          </li>
+        ))}
       </ol>
     </nav>
   );
-}
+};
+
+export default Breadcrumps;
