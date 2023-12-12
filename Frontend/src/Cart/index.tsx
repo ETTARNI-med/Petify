873e2f0fd53d07665f27c721f24b3e9ae68ac648
Salah Eddine EL MENYIY <76744964+SalahEddineELMENYIY@users.noreply.github.com>
2023-12-12 16:43:52 +0100

    final touches

--- Frontend/src/Cart/index.tsx
+++ Frontend/src/Cart/index.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import products from "./products";
 
 export default function Cart() {
-  const totalPrice = products.reduce((total, product) => total + product.price,0)
+  const totalPrice = products.reduce(
+    (total, product) => total + product.price,
+    0
+  );
   return (
     <section>
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
@@ -18,82 +21,78 @@ export default function Cart() {
               {products.map((product, index) => {
                 return (
                   <div className="flex flex-col space-y-2">
+                    <li className="flex items-center gap-4" key={index}>
+                      <img
+                        src={product.image}
+                        alt=""
+                        className="h-24 rounded object-cover"
+                      />
 
-                  <li className="flex items-center gap-4" key={index}>
-                    <img
-                      src={product.image}
-                      alt=""
-                      className="h-16 w-16 rounded object-cover"
-                    />
+                      <div>
+                        <h3 className="text-md md:text-lg xl:text-xl text-gray-900">
+                          {product.name}
+                        </h3>
 
-                    <div>
-                      <h3 className="text-md md:text-lg xl:text-xl text-gray-900">{product.name}</h3>
+                        <dl className="mt-0.5 ml-5 space-y-px text-md md:text-lg text-gray-600">
+                          <div>
+                            <dt className="inline">Size :</dt>
+                            <dd className="inline">{product.size}</dd>
+                          </div>
+                          <div className="text-md md:text-lg">
+                            <dt className="inline">Price :</dt>
+                            <dd className="inline">{product.price}</dd>
+                          </div>
 
-                      <dl className="mt-0.5 ml-5 space-y-px text-md md:text-lg text-gray-600">
-                        <div>
-                          <dt className="inline">Size :</dt>
-                          <dd className="inline">{product.size}</dd>
-                        </div>
-                        <div className="text-md md:text-lg">
-                          <dt className="inline">Price :</dt>
-                          <dd className="inline">{product.size}</dd>
-                        </div>
+                          <div>
+                            <dt className="inline">Color :</dt>
+                            <dd className="inline">{product.color}</dd>
+                          </div>
+                        </dl>
+                      </div>
 
-                        <div>
-                          <dt className="inline">Color :</dt>
-                          <dd className="inline">{product.color}</dd>
-                        </div>
-                      </dl>
-                    </div>
+                      <div className="flex flex-1 items-center justify-end gap-2">
+                        <form>
+                          <label htmlFor="Line1Qty" className="sr-only">
+                            {" "}
+                            Quantity{" "}
+                          </label>
 
-                    <div className="flex flex-1 items-center justify-end gap-2">
-                      <form>
-                        <label htmlFor="Line1Qty" className="sr-only">
-                          {" "}
-                          Quantity{" "}
-                        </label>
-
-                        <input
-                          type="number"
-                          min="1"
-                          value={product.quantity}
-                          id="Line1Qty"
-                          className="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-                        />
-                      </form>
-
-                      <button className="text-gray-600 transition hover:text-red-600">
-                        <span className="sr-only ">Remove item</span>
-
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="h-4 w-4"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                          <input
+                            type="number"
+                            min="1"
+                            value={product.quantity}
+                            id="Line1Qty"
+                            className="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
                           />
-                        </svg>
-                      </button>
-                    </div>
-                    
-                  </li>
-                  {
-                    index !== products.length - 1 ?  
-                    <hr className="h-px w-10/12 self-center my-8 dark:bg-gray-200 border-0 bg-gray-700"/>
-                    :
-                    null
-                  }
-                  </div>
+                        </form>
 
+                        <button className="text-gray-600 transition hover:text-red-600">
+                          <span className="sr-only ">Remove item</span>
+
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="h-4 w-4"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </li>
+                    {index !== products.length - 1 ? (
+                      <hr className="h-px w-10/12 self-center my-8 dark:bg-gray-200 border-0 bg-gray-700" />
+                    ) : null}
+                  </div>
                 );
               })}
-                    <hr className="h-px w-full self-center my-8 dark:bg-gray-200 border-0 bg-gray-700"/>
+              <hr className="h-px w-full self-center my-8 dark:bg-gray-200 border-0 bg-gray-700" />
             </ul>
 
             <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
@@ -102,8 +101,7 @@ export default function Cart() {
                   <div className="flex justify-between">
                     <div className="flex text-lg justify-between w-full">
                       <dt>Subtotal : </dt>
-                      <dd>{totalPrice} MAD
-                      </dd>
+                      <dd>{totalPrice} MAD</dd>
                     </div>
                   </div>
 
