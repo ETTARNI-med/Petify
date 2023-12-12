8dbc7e38d45598a25b9955d23d1053cd7cc34b43
labgenetic <Charafcharouit@gmail.com>
2023-12-12 16:17:57 +0100

    CartMade

--- Frontend/src/Cart/index.tsx
+++ Frontend/src/Cart/index.tsx
@@ -1,137 +1,164 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  addToCart,
-  clearCart,
-  decreaseCart,
-  getTotals,
-  removeFromCart,
-  YourProductType,
-} from "./cartSlice";
+import React from "react";
+import products from "./products";
 
-import { Link } from "react-router-dom";
-// import { RootState } from "./yourRootReducer"; // Update with the correct path to your root reducer
-
-const Cart: React.FC = () => {
-  const cart = useSelector((state) => state?.cart);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getTotals());
-  }, [cart, dispatch]);
-
-  const handleAddToCart = (product: YourProductType) => {
-    dispatch(addToCart(product));
-  };
-  const handleDecreaseCart = (product: YourProductType) => {
-    dispatch(decreaseCart(product));
-  };
-  const handleRemoveFromCart = (product: YourProductType) => {
-    dispatch(removeFromCart(product));
-  };
-  const handleClearCart = () => {
-    dispatch(clearCart());
-  };
-
+export default function Cart() {
+  const totalPrice = products.reduce((total, product) => total + product.price,0)
   return (
-    <div className="cart-container">
-      <h2>Shopping Cart</h2>
-      {cart.cartItems.length === 0 ? (
-        <div className="cart-empty">
-          <p>Your cart is currently empty</p>
-          <div className="start-shopping">
-            <Link to="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="currentColor"
-                className="bi bi-arrow-left"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
-                />
-              </svg>
-              <span>Start Shopping</span>
-            </Link>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <div className="titles">
-            <h3 className="product-title">Product</h3>
-            <h3 className="price">Price</h3>
-            <h3 className="quantity">Quantity</h3>
-            <h3 className="total">Total</h3>
-          </div>
-          <div className="cart-items">
-            {cart.cartItems &&
-              cart.cartItems.map((cartItem) => (
-                <div className="cart-item" key={cartItem.id}>
-                  <div className="cart-product">
-                    <img src={cartItem.image} alt={cartItem.name} />
+    <section>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+        <div className="mx-auto max-w-3xl">
+          <header className="text-center">
+            <h1 className="text-xl font-bold text-gray-900 sm:text-3xl">
+              My Cart
+            </h1>
+          </header>
+
+          <div className="mt-8 rounded-md backdrop-blur-sm bg-slate-100/60 p-4">
+            <ul className="space-y-4">
+              {products.map((product, index) => {
+                return (
+                  <div className="flex flex-col space-y-2">
+
+                  <li className="flex items-center gap-4" key={index}>
+                    <img
+                      src={product.image}
+                      alt=""
+                      className="h-16 w-16 rounded object-cover"
+                    />
+
                     <div>
-                      <h3>{cartItem.name}</h3>
-                      <p>{cartItem.desc}</p>
-                      <button onClick={() => handleRemoveFromCart(cartItem)}>
-                        Remove
+                      <h3 className="text-md md:text-lg xl:text-xl text-gray-900">{product.name}</h3>
+
+                      <dl className="mt-0.5 ml-5 space-y-px text-md md:text-lg text-gray-600">
+                        <div>
+                          <dt className="inline">Size :</dt>
+                          <dd className="inline">{product.size}</dd>
+                        </div>
+                        <div className="text-md md:text-lg">
+                          <dt className="inline">Price :</dt>
+                          <dd className="inline">{product.size}</dd>
+                        </div>
+
+                        <div>
+                          <dt className="inline">Color :</dt>
+                          <dd className="inline">{product.color}</dd>
+                        </div>
+                      </dl>
+                    </div>
+
+                    <div className="flex flex-1 items-center justify-end gap-2">
+                      <form>
+                        <label htmlFor="Line1Qty" className="sr-only">
+                          {" "}
+                          Quantity{" "}
+                        </label>
+
+                        <input
+                          type="number"
+                          min="1"
+                          value={product.quantity}
+                          id="Line1Qty"
+                          className="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+                        />
+                      </form>
+
+                      <button className="text-gray-600 transition hover:text-red-600">
+                        <span className="sr-only ">Remove item</span>
+
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="h-4 w-4"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                          />
+                        </svg>
                       </button>
                     </div>
+                    
+                  </li>
+                  {
+                    index !== products.length - 1 ?  
+                    <hr className="h-px w-10/12 self-center my-8 dark:bg-gray-200 border-0 bg-gray-700"/>
+                    :
+                    null
+                  }
                   </div>
-                  <div className="cart-product-price">${cartItem.price}</div>
-                  <div className="cart-product-quantity">
-                    <button onClick={() => handleDecreaseCart(cartItem)}>
-                      -
-                    </button>
-                    <div className="count">{cartItem.cartQuantity}</div>
-                    <button onClick={() => handleAddToCart(cartItem)}>+</button>
+
+                );
+              })}
+                    <hr className="h-px w-full self-center my-8 dark:bg-gray-200 border-0 bg-gray-700"/>
+            </ul>
+
+            <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
+              <div className="w-screen max-w-lg space-y-4">
+                <dl className="space-y-0.5 text-sm text-gray-700">
+                  <div className="flex justify-between">
+                    <div className="flex text-lg justify-between w-full">
+                      <dt>Subtotal : </dt>
+                      <dd>{totalPrice} MAD
+                      </dd>
+                    </div>
                   </div>
-                  <div className="cart-product-total-price">
-                    ${cartItem.price * cartItem.cartQuantity}
+
+                  <div className="flex justify-between">
+                    <dt>VAT</dt>
+                    <dd>4%</dd>
                   </div>
+
+                  <div className="flex justify-between">
+                    <dt>Discount</dt>
+                    <dd>-20 MAD</dd>
+                  </div>
+
+                  <div className="flex text-lg justify-between  font-medium">
+                    <dt>Total</dt>
+                    <dd>{totalPrice - 20}</dd>
+                  </div>
+                </dl>
+
+                <div className="flex justify-end">
+                  <span className="inline-flex items-center justify-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-indigo-700">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="-ms-1 me-1.5 h-4 w-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
+                      />
+                    </svg>
+
+                    <p className="whitespace-nowrap text-xs">
+                      2 Discounts Applied
+                    </p>
+                  </span>
                 </div>
-              ))}
-          </div>
-          <div className="cart-summary">
-            <button className="clear-btn" onClick={() => handleClearCart()}>
-              Clear Cart
-            </button>
-            <div className="cart-checkout">
-              <div className="subtotal">
-                <span>Subtotal</span>
-                <span className="amount">${cart.cartTotalAmount}</span>
-              </div>
-              <p>Taxes and shipping calculated at checkout</p>
-              <button>Check out</button>
-              <div className="continue-shopping">
-                <Link to="/">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="bi bi-arrow-left"
-                    viewBox="0 0 16 16"
+
+                <div className="flex justify-end">
+                  <a
+                    href="#"
+                    className="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
-                    />
-                  </svg>
-                  <span>Continue Shopping</span>
-                </Link>
+                    Checkout
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      )}
-    </div>
+      </div>
+    </section>
   );
-};
-
-export default Cart;
-
-// Replace YourProductType with the actual type of your product items
-// Replace RootState with the actual type of your Redux store state
+}
